6e9501dcdf4d826983636b04419ce2dad14a3cdc
dnsgk4983 <64947285+dnsgk4983@users.noreply.github.com>
2020-11-30 00:23:04 +0900

    123

--- js/main.js
+++ js/main.js
@@ -160,10 +160,12 @@ jQuery(document).ready(function($) {
 
 	var siteMenuClone = function() {
 
+
 		$('.js-clone-nav').each(function() {
 			var $this = $(this);
 			$this.clone().attr('class', 'site-nav-wrap').appendTo('.site-mobile-menu-body');
 		});
+		
 
 
 		setTimeout(function() {
